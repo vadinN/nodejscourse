56cc1e959473cab105dd73e527bfa267c6d5791b
vadinN <ninja.noob77@gmail.com>
2018-08-23 20:52:02 +0300

    WIP lesson5: изменил концептцию

--- lessons/lesson_5/app.js
+++ lessons/lesson_5/app.js
@@ -2,13 +2,32 @@
 
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
 const port = 3000;
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+/**
+ * todo: 
+ * GET /
+ * GET /list
+ * GET /watch
+ * POST /upload
+ */
+
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('index', {
+    message: 'Hello world!'
+  });
 });
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}`);
 });
+
+
+
